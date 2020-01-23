1f87529f48c286068ad1f545a54a501a5175a3db
Red-mar <redmarvanderiet@gmail.com>
2020-01-23 22:18:01 +0100

    split dbus

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::process;
 
 use serde::{Deserialize, Serialize};
 
-use dbus::{arg, blocking::Connection};
-use std::collections::HashMap;
-use std::time::Duration;
+use i3bar_cmus::get_cmus;
 
 #[derive(Serialize, Deserialize)]
 struct BarMessage<'a> {
@@ -28,47 +26,6 @@ fn read_line(line: &mut String) {
     }
 }
 
-fn ret_refarg(value: &dyn arg::RefArg) -> &str {
-    let mut ret = "Unknown";
-    if let Some(string) = value.as_str() {
-        ret = string;
-    }
-    // why
-    else if let Some(ite) = value.as_iter() {
-        for a in ite {
-            if let Some(a_i) = a.as_iter() {
-                for ab in a_i {
-                    if let Some(a_s) = ab.as_str() {
-                        ret = a_s;
-                    }
-                }
-            }
-        }
-    }
-    ret
-}
-
-fn dbus_m(artist: &mut String, title: &mut String) {
-    let conn = Connection::new_session().unwrap();
-    let proxy = conn.with_proxy(
-        "org.mpris.MediaPlayer2.cmus",
-        "/org/mpris/MediaPlayer2",
-        Duration::from_millis(5000),
-    );
-    use dbus::blocking::stdintf::org_freedesktop_dbus::Properties;
-    let a: HashMap<String, arg::Variant<Box<dyn arg::RefArg>>> = proxy
-        .get("org.mpris.MediaPlayer2.Player", "Metadata")
-        .unwrap();
-    for (k, v) in a {
-        if k == "xesam:artist" {
-            *artist = ret_refarg(&v).to_string();
-        }
-        if k == "xesam:title" {
-            *title = ret_refarg(&v).to_string();
-        }
-    }
-}
-
 fn main() {
     let mut input = String::new();
     read_line(&mut input);
@@ -83,7 +40,7 @@ fn main() {
 
         let mut artist = String::new();
         let mut title = String::new();
-        dbus_m(&mut artist, &mut title);
+        get_cmus(&mut artist, &mut title);
 
         let m = BarMessage {
             name: "test",
